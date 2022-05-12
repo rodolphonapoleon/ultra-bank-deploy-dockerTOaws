754dbd1b4018c1317ed6c786bf5b8b5c0b33943e
rodolphonapoleon <rnapoleon@imajine.ht>
2022-05-12 12:06:33 -0400

    updated instance name

--- bin/awsec3_express_docker.ts
+++ bin/awsec3_express_docker.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import cdk = require("@aws-cdk/core");
-import { UltraBankBackendDocker } from "../lib/awsec3_express_docker-stack";
+import { UltraBankBackend } from "../lib/awsec3_express_docker-stack";
 
 const app = new cdk.App();
 const envUSA = { account: "350776635962", region: "us-east-1" };
 
-new UltraBankBackendDocker(app, "UltraBankBackendDocker", { env: envUSA });
+new UltraBankBackend(app, "UltraBankBackend", { env: envUSA });
 app.synth();
