f87c5859e7cc3a4a9213fbbf5ff931f0db0fd315
rodolphonapoleon <rnapoleon@imajine.ht>
2022-05-25 17:28:15 -0400

    Deployment e

--- lib/awsec3_express_docker-stack.ts
+++ lib/awsec3_express_docker-stack.ts
@@ -25,7 +25,7 @@ export class UltraBankBackend extends cdk.Stack {
 
     const helloContainer = helloTaskDefinition.addContainer("hello", {
       image: ecs.ContainerImage.fromRegistry(
-        "0508198705081987/test-bank-backend-d"
+        "0508198705081987/test-bank-backend-e"
       ),
       memoryLimitMiB: 128,
     });
